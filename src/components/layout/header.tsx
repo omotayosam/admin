66a364049de63fc9989531e26edaeb58d7573bbe
omotayosam <Ajfuzzylogic@gmail.com>
2025-08-11 23:29:00 +0100

    reverted user import in header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,7 +7,6 @@ import SearchInput from '../search-input';
 import { ThemeSelector } from '../theme-selector';
 import { ModeToggle } from './ThemeToggle/theme-toggle';
 import CtaGithub from './cta-github';
-import { UserNav } from './user-nav';
 
 export default function Header() {
   return (
@@ -26,7 +25,6 @@ export default function Header() {
 
         <ModeToggle />
         <ThemeSelector />
-        <UserNav />
       </div>
     </header>
   );
